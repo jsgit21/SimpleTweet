30d6905441f15fd6b46dbf2b32dabf13d93acc7d
Joseph Santantonio <js958@njit.edu>
2021-09-29 14:04:19 -0400

    Finishing touches

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -67,6 +67,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         ImageView ivProfileImage;
         TextView tvBody;
         TextView tvScreenName;
+        TextView tvRelativeTime;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -74,12 +75,17 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvBody = itemView.findViewById(R.id.tvBody);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvRelativeTime = itemView.findViewById(R.id.tvRelativeTime);
         }
 
         public void bind(Tweet tweet) {
             tvBody.setText(tweet.body);
             tvScreenName.setText(tweet.user.screenName);
-            Glide.with(context).load(tweet.user.profileImageUrl).circleCrop().into(ivProfileImage);
+            tvRelativeTime.setText(tweet.createdAt);
+            Glide.with(context).load(tweet.user.profileImageUrl)
+                    .circleCrop()
+                    .placeholder(R.drawable.placeholder)
+                    .into(ivProfileImage);
         }
     }
 }
